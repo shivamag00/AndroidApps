8d75d9b95a47408f114a5a5aa6f8aa7072fc99bc
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2020-06-29 14:35:39 +0530

    Added Images for each PokeMon

--- Pokedex/app/src/main/java/com/example/pokedex/PokemonActivity.java
+++ Pokedex/app/src/main/java/com/example/pokedex/PokemonActivity.java
@@ -2,10 +2,14 @@ package com.example.pokedex;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -18,16 +22,22 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class PokemonActivity extends AppCompatActivity {
     private TextView nameTextView;
     private TextView numberTextView;
     private TextView type1TextView;
     private TextView type2TextView;
     private String url;
+    private String image_url;
     private String name;
     private RequestQueue requestQueue;
     private Catcher preferences;
     Button button;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +62,7 @@ public class PokemonActivity extends AppCompatActivity {
         numberTextView = findViewById(R.id.pokemon_number);
         type1TextView = findViewById(R.id.pokemon_type1);
         type2TextView = findViewById(R.id.pokemon_type2);
+        imageView = findViewById(R.id.pokemon_image);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -86,6 +97,10 @@ public class PokemonActivity extends AppCompatActivity {
                             type2TextView.setText(type);
                         }
                     }
+
+                    JSONObject imageEntry = response.getJSONObject("sprites");
+                    image_url = imageEntry.getString("front_default");
+                    new DownloadSpriteTask().execute(image_url);
                 } catch (JSONException e) {
                     Log.e("cs50", "Pokemon json error", e);
                 }
@@ -112,5 +127,24 @@ public class PokemonActivity extends AppCompatActivity {
             button.setText("Release");
         }
 
+    }
+
+    private class DownloadSpriteTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                return BitmapFactory.decodeStream(url.openStream());
+            }
+            catch (IOException e) {
+                Log.e("cs50", "Download sprite error", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
