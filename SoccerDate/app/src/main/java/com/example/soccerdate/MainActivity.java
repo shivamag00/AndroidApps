aec9bbf8cc0323ce5509d132454f4b9ae4d89342
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2020-07-04 22:25:42 +0530

    Setting the title name according to the bottom navigation

--- SoccerDate/app/src/main/java/com/example/soccerdate/MainActivity.java
+++ SoccerDate/app/src/main/java/com/example/soccerdate/MainActivity.java
@@ -6,12 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     public BottomNavigationView itemView;
+    public MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         itemView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        toolbar = (MaterialToolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Live Score");
+
         itemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.page_1:
+                        toolbar.setTitle("Live Score");
                         break;
 
                     case R.id.page_2:
+                        toolbar.setTitle("Stats");
                         break;
                 }
                 return true;
             }
         });
+
+
     }
 }
