f4a85e3db679780e927a44c48795d2cda83cde6e
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2020-07-04 21:23:42 +0530

    Added Bottom and Top Navigation Bar Layouts

--- SoccerDate/app/src/main/java/com/example/soccerdate/MainActivity.java
+++ SoccerDate/app/src/main/java/com/example/soccerdate/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.soccerdate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public BottomNavigationView itemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        itemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.page_1:
+                        break;
+
+                    case R.id.page_2:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
